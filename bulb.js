00dec9c23506b03cc386f72c8dbc024691c5c927
Nanda Kishore <nameisnanda3@gmail.com>
2026-02-13 18:00:49 +0530

    Add files via upload

---
+++ bulb.js
@@ -0,0 +1,15 @@
+function off_switch() {
+    document.getElementById("bulbImg").src = "https://d2clawv67efefq.cloudfront.net/ccbp-dynamic-webapps/bulb-go-off-img.png";
+    document.getElementById("catImg").src = "https://d2clawv67efefq.cloudfront.net/ccbp-dynamic-webapps/cat-eyes-img.png";
+    document.getElementById("switchStatus").textContent = "Switched Off";
+    document.getElementById("onSwitch").style.backgroundColor = "#22c55e";
+    document.getElementById("offSwitch").style.backgroundColor = "#cbd2d9";
+}
+
+function on_switch() {
+    document.getElementById("bulbImg").src = "https://d2clawv67efefq.cloudfront.net/ccbp-dynamic-webapps/bulb-go-on-img.png";
+    document.getElementById("catImg").src = "https://d2clawv67efefq.cloudfront.net/ccbp-dynamic-webapps/cat-img.png";
+    document.getElementById("switchStatus").textContent = "Switched On";
+    document.getElementById("offSwitch").style.backgroundColor = "#e12d39";
+    document.getElementById("onSwitch").style.backgroundColor = "#cbd2d9";
+}
